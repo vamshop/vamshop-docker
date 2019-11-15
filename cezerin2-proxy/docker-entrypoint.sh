b68143585d4410bc975a532f2200e7181df879ff
vam <vamshop@gmail.com>
2019-11-15 19:35:34 +0300

    Fixes

--- cezerin2-proxy/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-envsubst '${DOMAIN} ${STORE_HOST} ${ADMIN_HOST} ${API_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-nginx -g "daemon off;"
